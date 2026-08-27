537385e0b1d02f6d4f6f74a9ce5aa997f8dae178
Neverita-c <vova75486@gmail.com>
2026-08-27 20:42:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ import threading
 import telebot
 from telebot import types
 
-# --- 1. ТОКЕН ТВОЕГО БОТА ---
+# --- 1. НАСТРОЙКИ ТВОЕГО БОТА ---
 BOT_TOKEN = "8669188850:AAEDTy2I8Z9jR11AKjbHI9t6TJ8YJbQTLWU"
+
+# СЮДА ВСТАВЬ СВОЙ ТОКЕН, КОТОРЫЙ ТЫ ПОЛУЧИЛ ОТ PAYMASTER (ВНУТРЬ КАВЫЧЕК)
+PAYMENT_PROVIDER_TOKEN = "1744374395:TEST:d120a3e7495fe4dbc7c5" 
 
 # --- 2. ФАЛЬШИВЫЙ СЕРВЕР ДЛЯ ХОСТИНГА (ЧТОБЫ НЕ БЫЛО TIMED OUT) ---
 def start_dummy_server():
@@ -24,7 +27,6 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
-    # Ссылка на твою игру с правильными большими буквами
     game_button = types.InlineKeyboardButton(
         text="🚀 Играть в Биткоин", 
         web_app=types.WebAppInfo(url="https://github.io")
@@ -41,15 +43,16 @@ def send_welcome(message):
 @bot.message_handler(content_types=['web_app_data'])
 def web_app_data_receive(message):
     if message.web_app_data.data == "donate_1m":
-        prices = [types.LabeledPrice(label="1 000 000 Биткоинов", amount=50)] 
+        # Цена: 49 рублей (в копейках это 4900)
+        prices = [types.LabeledPrice(label="1 000 000 Биткоинов", amount=4900)] 
         
         bot.send_invoice(
             chat_id=message.chat.id,
             title="⚡ 1 000 000 Биткоинов",
             description="Моментальное зачисление одного миллиона монет на твой баланс в игре!",
             invoice_payload="donate_1m_payload",
-            provider_token="", 
-            currency="XTR",   
+            provider_token=PAYMENT_PROVIDER_TOKEN, # Твой рублевый шлюз
+            currency="RUB",   # Меняем валюту на рубли РФ
             prices=prices
         )
 
@@ -61,7 +64,7 @@ def checkout(pre_checkout_query):
 def got_payment(message):
     bot.send_message(
         message.chat.id, 
-        "🔥 Оплата прошла успешно! 1 000 000 Биткоинов начислены! (После 31 августа на ПК мы настроем базу данных, чтобы они сохранялись на балансе)."
+        "🔥 Оплата прошла успешно! 1 000 000 Биткоинов начислены! (После 31 августа на ПК настроим базу, чтобы они отображались на балансе)."
     )
 
 if __name__ == '__main__':
